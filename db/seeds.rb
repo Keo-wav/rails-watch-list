428306bfcd299c9a73540bce328dad471e45b49f
nicolas nackphouminh <n.nackphouminh@gmail.com>
2023-02-18 03:18:15 +0100

    seeding really done

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+Bookmark.destroy_all
+List.destroy_all
 Movie.destroy_all
 
 require "open-uri"
